10452d35b89ce0be753abeac0c291d175bbec1c6
AAmen <dtymnn@gmail.com>
2020-11-20 20:11:16 +0530

    Creep patrolling. Player movement(incomplete).
    Player movement(incomplete), enemy patrolling.

---
+++ ProjectDS/Assets/Scripts/creepwalk.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class creepwalk : MonoBehaviour
+{
+    public Camera cam;
+    private NavMeshAgent agent;
+    public Transform player;
+    public Transform parentCheckpoints;
+    private List<Transform> checkpoints = new List<Transform>();
+    private int nextCheckpoint;
+    private bool hasAcquiredPlayer;
+    // Update is called once per frame
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        for (int i = 0; i < parentCheckpoints.childCount; i++)
+        {
+            Debug.Log(parentCheckpoints.GetChild(i).name);
+            checkpoints.Add(parentCheckpoints.GetChild(i));
+        }
+        nextCheckpoint = 0;
+        hasAcquiredPlayer = false;
+    }
+    void Update()
+    {        
+        if (!hasAcquiredPlayer)
+        {
+            Patrol();
+        }     
+    }
+    void Patrol()
+    {
+        if(!agent.hasPath && checkpoints != null)
+        {
+            agent.SetDestination(checkpoints[nextCheckpoint].position);
+            nextCheckpoint++;
+            if (nextCheckpoint >= checkpoints.Count)
+            {
+                nextCheckpoint = 0;
+            }
+        }   
+    }
+}
