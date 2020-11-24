c011f28346a55c6ad7a744bb8c1979cf7156c037
AAmen <adityasureshmenon@gmail.com>
2020-11-24 19:34:10 +0530

    Enemy AI line of sight using raycasts. EnemyStatsClass.
    Implementation of lineOfSight(), turn smoothing on engaged, stats fixing.

--- ProjectDS/Assets/Scripts/creepwalk.cs
+++ ProjectDS/Assets/Scripts/creepwalk.cs
@@ -2,43 +2,162 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using System.Linq;
 public class creepwalk : MonoBehaviour
 {
+    #region Enemy derived calculations
+    public class Stats{
+        public int health;
+        public float stamina;
+        public float baseDamage;
+    }
+    static Stats creepstats;
+    public Transform eyes;
+    #endregion
+
+    #region transforms
     public Camera cam;
     private NavMeshAgent agent;
     public Transform player;
     public Transform parentCheckpoints;
     private List<Transform> checkpoints = new List<Transform>();
     private int nextCheckpoint;
+
+    #endregion
+
+    #region player derived calculations
+
+    [SerializeField]
     private bool hasAcquiredPlayer;
-    // Update is called once per frame
+    [SerializeField]
+    private float distance;
+    public float distanceFromPlayer;
+    public Transform playerNeck;
+    public float waitTime = 2f;
+    [SerializeField]
+    private bool los = false;
+    private float angularSpeed;
+    #endregion
+
+    #region misc
+    public LayerMask whatToHit;
+    #endregion
     void Start() {
         agent = GetComponent<NavMeshAgent>();
         for (int i = 0; i < parentCheckpoints.childCount; i++)
-        {
-            Debug.Log(parentCheckpoints.GetChild(i).name);
+        {            
             checkpoints.Add(parentCheckpoints.GetChild(i));
         }
         nextCheckpoint = 0;
-        hasAcquiredPlayer = false;
+        hasAcquiredPlayer = false;        
+        creepstats = new Stats();
+        angularSpeed = agent.angularSpeed;        
+
     }
     void Update()
-    {        
-        if (!hasAcquiredPlayer)
-        {
-            Patrol();
-        }     
-    }
-    void Patrol()
     {
-        if(!agent.hasPath && checkpoints != null)
-        {
-            agent.SetDestination(checkpoints[nextCheckpoint].position);
-            nextCheckpoint++;
-            if (nextCheckpoint >= checkpoints.Count)
+        
+        StartCoroutine(Patrol());        
+        distance = Vector3.Distance(player.position, transform.position);
+        
+        if (distance < distanceFromPlayer)
+        {            
+            hasAcquiredPlayer  = true;                
+        }
+        else if(distance > 3 * distanceFromPlayer)
+        {            
+            hasAcquiredPlayer = false;
+            los = false;
+            agent.angularSpeed = angularSpeed;
+            StartCoroutine(Patrol());            
+        } 
+    }
+    IEnumerator Patrol()
+    {
+        if (hasAcquiredPlayer && !los)
+        {            
+            StartCoroutine(waitOnNext());
+            los = inLineOfSight();
+            if (los)
             {
-                nextCheckpoint = 0;
+                StartCoroutine(Engage());
+            }                                   
+        }
+        if(!agent.hasPath && checkpoints != null)
+        {           
+            nextCheckpoint = Random.Range(0, checkpoints.Count);               
+            agent.SetDestination(checkpoints.ElementAt(nextCheckpoint).position);            
+            yield return new WaitForSeconds(waitTime);            
+            StartCoroutine(Patrol());
+        }
+    }
+
+    IEnumerator Engage()
+    {
+        agent.angularSpeed = 0;
+        // TODO: Also check if stamina > 0.
+        if (agent.pathStatus == NavMeshPathStatus.PathComplete && creepstats.stamina > 0)
+        {            
+            agent.stoppingDistance = 4f;
+            agent.SetDestination(player.position);
+            // Attack?
+        }
+
+
+        if (hasAcquiredPlayer)
+        {
+            faceTarget(player.position);
+            agent.stoppingDistance = 10f;            
+            agent.SetDestination(player.position);
+            // Set animation to walk/run?
+            yield return new WaitForSeconds(1f);                        
+            StartCoroutine(Engage());
+        }        
+        else
+        {
+            Debug.Log("Stopping");            
+            agent.stoppingDistance = 0f;
+            agent.ResetPath();            
+        }                                
+    }
+
+    private bool inLineOfSight()
+    {        
+        if(los)
+        {
+            return true;
+        }
+        Vector3 fromPosition = eyes.transform.position;
+        Vector3 toPosition = playerNeck.transform.position;
+        Vector3 direction = toPosition - fromPosition;
+        direction.Normalize();
+        
+        RaycastHit hit;
+        if (Physics.Raycast(fromPosition, direction, out hit, distanceFromPlayer))
+        {
+            Debug.DrawLine(fromPosition, hit.transform.position, Color.red);
+            if (hit.collider.name == "Player")
+            {
+                return true;
             }
-        }   
+            else
+            {
+                return false;
+            }
+        }        
+        return false;
+    }
+
+    private void faceTarget(Vector3 target)
+    {
+        Vector3 lookAt = target - transform.position;
+        lookAt.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(lookAt);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.5f);
+    }
+
+    IEnumerator waitOnNext()
+    {
+        yield return new WaitForSeconds(waitTime);        
     }
 }
