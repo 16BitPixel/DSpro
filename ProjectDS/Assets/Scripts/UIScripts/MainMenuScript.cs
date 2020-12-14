656dfd3ddd335d7f9aed44425ac98d9ad93854f1
shantanu-shukla <53634974+shatanu-shukla@users.noreply.github.com>
2020-12-14 21:49:23 +0530

    Basic Main menu UI added...

---
+++ ProjectDS/Assets/Scripts/UIScripts/MainMenuScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject MainPanel;
+    public GameObject SettingsPanel;
+
+    private void Start()
+    {
+        MainPanel.SetActive(true);
+        SettingsPanel.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void EnableSettingsUI()
+    {
+        MainPanel.SetActive(false);
+        SettingsPanel.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+
+    //Temporarily placed here until settings UI is implemented properly
+    public void BackButton()
+    {
+        MainPanel.SetActive(true);
+        SettingsPanel.SetActive(false);
+    }
+}
