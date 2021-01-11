58b123e0ac76aa24e1964a9e96d62e76fb61db0e
Shantanus-Aavega <shantanus@suntechnologies.com>
2021-01-11 17:43:38 +0530

    Pause menu UI designed for gameplay scene. (Currently not functional......)

---
+++ ProjectDS/Assets/Scripts/UIScripts/GameplayScene/PauseScreenScrpt.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScreenScrpt : MonoBehaviour
+{
+    public GameObject PausePanel, settingsPanel, gameplayPanel;
+
+    public static bool gameIsPaused;
+
+
+    void Start()
+    {
+        gameplayPanel.SetActive(true);
+        PausePanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+
+            else
+            {
+                Pause();
+            }
+
+        }
+    }
+
+    public void Resume()
+    {
+        gameplayPanel.SetActive(true);
+        PausePanel.SetActive(false);
+        settingsPanel.SetActive(false);
+
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        gameplayPanel.SetActive(false);
+        PausePanel.SetActive(true);
+        settingsPanel.SetActive(false);
+
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void QuitToMainMenu()
+    {
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+
+        SceneManager.LoadScene(0);
+    }
+
+    public void EnableSettingsUI()
+    {
+        PausePanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void BackToPauseScreen()
+    {
+        PausePanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+
+}
---
+++ ProjectDS/Assets/Scripts/UIScripts/UiInputScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiInputScript : MonoBehaviour
+{
+    public static UiInputScript UIInpScrpt;
+
+      PlayerControls inputActions;
+    GameObject canvas;
+
+
+    private void Start()
+    {
+        if (UIInpScrpt == null)
+        {
+            UIInpScrpt = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void invokePause()
+    {
+       // bool isOpen = false;
+       // bool pauseInvoked = inputActions.PlayerActions.Pause.phase == UnityEngine.InputSystem.InputActionPhase.Performed;
+
+        /*if (pauseInvoked)
+        {
+            canvas.SetActive(!isOpen);
+            isOpen = !isOpen;
+        }*/
+
+    }
+}
